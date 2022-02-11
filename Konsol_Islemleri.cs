fd60231462b90354108f32a393a5d68d032241da
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-11 18:35:13 +0300

    Konsol_Islemleri sınıfında GirisMesaji ve CumleleriAlma adlı 2 tane metot kuruldu.

--- Konsol_Islemleri.cs
+++ Konsol_Islemleri.cs
@@ -1,7 +1,18 @@
+using System;
 namespace KarakterTerstenYazdırma
 {
     public class Konsol_Islemleri
     {
+        public void GirisMesaji() // Talimatları yazdıracak metot.
+        {
+            System.Console.WriteLine("Lütfen girmek istediğiniz cümleleri aralarında boşluk olacak şekilde yazınız: "); 
+        }
+        public string[] CumleleriAlma()
+        {
+            string cumleler= Console.ReadLine();
+            return cumleler.Split(" "); // aralarındaki boşluk sayesinde cümleleri birbirinden ayırıp bir string diziye atadım ve bunu döndürdüm. 
+            
+        }
         
     }
 }
